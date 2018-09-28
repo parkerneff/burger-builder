0093b2c63a2c84f224ef1dfaf3e1e0e8fa879110
pneff <pneff@unicon.net>
2018-09-28 10:00:43 -0700

    135

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import burgerLogo from '../../assets/images/burger-logo.png';
 import classes from './Logo.css';
 
 const logo = (props) => (
-    <div className={classes.Logo}><img src={burgerLogo} alt="A Hamburger"  /></div>
+    <div className={classes.Logo} style={{height: props.height}}><img src={burgerLogo} alt="A Hamburger"  /></div>
 );
 
 export default logo;
