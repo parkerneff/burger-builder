45f4c3dea21b3a02e794658becce15db103e45da
pneff <pneff@unicon.net>
2018-09-27 17:01:01 -0700

    130

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -108,6 +108,7 @@ class BurgerBuilder extends Component {
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     <OrderSummary ingredients={this.state.ingredients}
+                                  price={this.state.totalPrice.toFixed(2)}
                                   purchaseCancelled={this.purchaseCancelHandler}
                                   purchaseContinued={this.purchaseContinueHandler}/>
                 </Modal>
