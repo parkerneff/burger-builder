f0e01183f735b2145a5ac0aed422001519fa5d7e
pneff <pneff@unicon.net>
2018-10-17 15:14:10 -0700

    201 Setting up routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
+import {Route} from 'react-router-dom';
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
+import Switch from "react-router-dom/es/Switch";
 
 
 class App extends Component {
@@ -9,8 +11,11 @@ class App extends Component {
         return (
             <div>
                 <Layout>
-                    <BurgerBuilder/>
-                    <Checkout/>
+                    <Switch>
+                        <Route path="/checkout" component={Checkout}/>
+                        <Route path="/" exact  component={BurgerBuilder}/>
+                    </Switch>
+
                 </Layout>
             </div>
         );
